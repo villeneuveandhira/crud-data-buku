7178b4c2d4c309b2cabd4dbd508b40fba15ba1d1
Vil <101118033+villeneuveandhira@users.noreply.github.com>
2022-05-25 08:26:11 +0700

    Delete tp6 directory

--- tp6/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con  = mysqli_connect('localhost','root','','web_tp6');
-if(mysqli_connect_errno())
-{
-    echo 'Database Connection Error';
-}
--- tp6/get_single_data.php
+++
@@ -1,7 +0,0 @@
-<?php include('connection.php');
-$id = $_POST['id'];
-$sql = "SELECT * FROM books WHERE id='$id' LIMIT 1";
-$query = mysqli_query($con,$sql);
-$row = mysqli_fetch_assoc($query);
-echo json_encode($row);
-?>
